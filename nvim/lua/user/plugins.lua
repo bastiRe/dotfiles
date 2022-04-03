80c890790eadfe6585c11aa9eb68c268c1cb4c97
Sebastian Rehm <rehmsebastian@gmail.com>
2022-04-03 17:36:02 +0200

    Remove superflouos templates

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -62,8 +62,6 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use 'shaunsingh/nord.nvim'
-  use 'shaunsingh/solarized.nvim'
   use 'sainnhe/gruvbox-material'
 
   -- cmp plugins
